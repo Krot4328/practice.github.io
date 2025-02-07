9cc7e17a6ba93f09c442a6ea0bc5dc180f2a2d26
Krot4328 <86371996+Krot4328@users.noreply.github.com>
2025-02-07 13:22:42 +0200

    Commit Day 5

--- js/app/app.js
+++ js/app/app.js
@@ -3,6 +3,7 @@ import { header } from './widgets/header.js';
 import { popup } from './widgets/popup.js';
 import { msg } from './widgets/msg.js';
 import { toogle } from './widgets/toogle.js';
+import { img } from './widgets/img.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const main = {
@@ -93,6 +94,7 @@ document.addEventListener('DOMContentLoaded', function() {
     .component('popup', popup)
     .component('msg', msg)
     .component('toogle', toogle)
+    .component('Image', img)
     .use(router)
     .mount('#content')
 });
